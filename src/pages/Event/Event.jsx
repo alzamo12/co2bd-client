5603b68e73d4017df15cdc1b8387cce22be98f62
alzamo12 <rafiqulislam4969@gmail.com>
2025-06-14 21:54:49 +0600

    get event data after clicking on joined event with user email and name

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../hooks/useAuth";
 
 const Event = () => {
     const params = useParams();
     const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
 
     const { data: event } = useQuery({
         queryKey: ["event"],
@@ -14,8 +16,18 @@ const Event = () => {
         }
     })
 
-    const {image, title, description} = event;
+    const { image, title, description } = event;
 
+    const handleJoinEvent = event => {
+        const { _id, ...rest } = event;
+        const eventData = {
+            eventId: _id,
+            ...rest,
+            user_email: user.email,
+            user_name: user.displayName
+        };
+        console.log(eventData)
+    }
 
     return (
         <div className="card lg:card-side bg-base-100 shadow-sm mt-20">
@@ -28,7 +40,7 @@ const Event = () => {
                 <h2 className="card-title">{title}!</h2>
                 <p>{description}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-neutral">Join Event</button>
+                    <button onClick={() => handleJoinEvent(event)} className="btn btn-neutral">Join Event</button>
                 </div>
             </div>
         </div>
