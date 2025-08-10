7237d28b35f82bb0e10cf0cb7dc7b44899345a91
alzamo12 <rafiqulislam4969@gmail.com>
2025-08-10 19:51:30 +0600

    add server url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    // baseURL: "https://co2bd-server.vercel.app/"
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://co2bd-server.vercel.app/"
+    // baseURL: "http://localhost:5000/"
 });
 
 const useAxiosSecure = () => {
